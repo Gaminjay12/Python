18782eeab8a5646232faf32cd7e2c20fb1199c34
The Jay <mastercrafterhayden@outlook.com>
2025-01-20 18:26:36 +0000

    cool

---
+++ utility/utility.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+os.system("clear")
+
+print("====================================")
+print("=         UTILITY                 =")
+print("====================================")
+while True:
+    
+    user_input = input("> ")
+    if user_input == "exit":
+        break
+    if user_input == "help":
+        import os
+        os.system("clear")
+        print("=============")
+        print("=    HELP   =")
+        print("=============")
+
