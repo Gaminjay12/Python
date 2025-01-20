d0e879ab727bd5c04416fdd84ffcaa426a7c6785
The Jay <mastercrafterhayden@outlook.com>
2025-01-20 09:55:51 -0600

    utility v0.1

---
+++ utility.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+while True:
+    print("====================================")
+    print("=         UTILITY                 =")
+    print("====================================")
+    user_input = input("> ")
+    if user_input == "exit":
+        break
+    if user_input == "help":
+        print("exit - exit the program")
+        print("help - display this help")
